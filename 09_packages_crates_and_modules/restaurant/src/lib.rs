a7cd021cb268a1c1a4a33dc4d3d3818e31cdaaa8
Krzysztof Brzozowski <krzysztof_brzozowski@icloud.com>
2024-09-24 18:15:03 +0200

    Added few demos

--- 09_packages_crates_and_modules/restaurant/src/lib.rs
+++ 09_packages_crates_and_modules/restaurant/src/lib.rs
@@ -14,10 +14,60 @@ mod front_of_house {
     }
 }
 
+fn deliver_order() {}
+
+mod back_of_house {
+    // Public enum demo
+    // In contrast, if we make an enum public, all of its variants are then public. We only need the pub before the enum keyword
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    // Super call demo
+    fn fix_incorrect_order() {
+        cook_order();
+        // Calling parent module with super
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
+
+    // Private field demo
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+}
+
 pub fn eat_at_restaurant() {
-    // Absolute path to call add_to_waitlist
+    // Public enum demo
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+
+    // Absolute path to call add_to_waitlist demo
     crate::front_of_house::hosting::add_to_waitlist();
 
-    // Relative path to call add_to_waitlist
+    // Relative path to call add_to_waitlist demo
     front_of_house::hosting::add_to_waitlist();
+
+    // Private field demo
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal
+    // meal.seasonal_fruit = String::from("blueberries");
 }
--- README.md
+++ README.md
@@ -75,16 +75,30 @@ backyard
     └── main.rs
 ```
 ### restaurant
+Basically demos for
+- Modules in the same lib
+- Public enum
+- Super call
+- Private field
 ```
 crate
  └── front_of_house
-     ├── hosting
-     │   ├── add_to_waitlist
-     │   └── seat_at_table
-     └── serving
-         ├── take_order
-         ├── serve_order
-         └── take_payment
+ │   ├── hosting
+ │   │   ├── add_to_waitlist
+ │   │   └── seat_at_table
+ │   └── serving
+ │       ├── take_order
+ │       ├── serve_order
+ │       └── take_payment
+ └── back_of_house
+     ├── deliver_order
+     └── back_of_house
+         ├── fix_incorrect_order
+         └── cook_order
+    ...
+    +
+    Some other implementations
+
 ```
 This tree shows how some of the modules nest inside other modules; for example, hosting nests inside front_of_house. The tree also shows that some modules are siblings, meaning they’re defined in the same module; hosting and serving are siblings defined within front_of_house. If module A is contained inside module B, we say that module A is the child of module B and that module B is the parent of module A. Notice that the entire module tree is rooted under the implicit module named crate.
 
@@ -94,4 +108,7 @@ Build with
 ```
 cargo build
 ```
+
+
+
 
