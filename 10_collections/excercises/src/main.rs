86ade1fc8ec5556787903ffa37dc737a7f58f890
Krzysztof Brzozowski <krzysztof_brzozowski@icloud.com>
2024-09-28 10:50:44 +0200

    Modified a little example - adding functions

--- 10_collections/excercises/src/main.rs
+++ 10_collections/excercises/src/main.rs
@@ -4,27 +4,49 @@ fn main() {
     // Given a list of integers, use a vector and return 
     // the median (when sorted, the value in the middle position) 
     // and mode (the value that occurs most often; a hash map will be helpful here) of the list.
+    
     // 1. Median
     let mut list = vec![1, 5, 12, 8, 5, 9, 14, 88];
     list.sort();
 
-    let idx = list.len() / 2;
+    let median = median(&list);
+    println!("median is {median:#?}");
 
+    // 2. Mode
+    let mode = mode(&list);
+    println!("Max occurance has {mode:#?}");
+}
+
+fn median(list: &Vec<i32>) -> Option<i32> {
+    if list.len() == 0 {
+        // let median: Option<i32> = None;
+        return None;
+    }
+
+    let idx = list.len() / 2;
     println!("Sorted list is {list:#?}");
 
     match list.len() % 2 {
         // If devide is even number
-        0 => println!("median is {}", 
-                &list[idx - 1] + &list[idx]),
+        0 => {
+            let median = &list[idx - 1] + &list[idx];
+            return Some(median);
+        },
 
-        1 => println!("median is {}", 
-                &list[idx]),
+        1 => {
+            let median = &list[idx];
+            return Some(*median);
+        },
 
-        _ => {},
+        _ => None,
+    }
+}
 
+fn mode(list: &Vec<i32>) -> Option<i32> {
+    if list.len() == 0 {
+        return None;
     }
 
-    // 2. Mode
     let mut map = HashMap::new();
     for item in list {
         let num = map.entry(item).or_insert(0);
@@ -33,5 +55,6 @@ fn main() {
     println!("Map is {map:#?}");
     // TODO - do not understand, copied that from stack
     let max_val = map.iter().max_by_key(|entry | entry.1).unwrap();
-    println!("Max occurance has {} -> {}", max_val.0, max_val.1);
+
+    Some(**max_val.0)
 }
