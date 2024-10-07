b182a05266f8b98623f5a29b04cc5b8d3104e3a4
Krzysztof Brzozowski <krzysztof_brzozowski@icloud.com>
2024-10-07 19:58:52 +0200

    Small bump

--- 14_io_project_grep/minigrep/src/main.rs
+++ 14_io_project_grep/minigrep/src/main.rs
@@ -5,13 +5,24 @@ fn main() {
     // Iterators produce a series of values, and we can call the collect method on an iterator to turn it into a collection
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = parse_config(&args);
 
-    println!("Searching for {query}");
-    println!("In file {file_path}");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let file_content = fs::read_to_string(file_path).expect("Uppps");
+    let file_content = fs::read_to_string(config.file_path).expect("Uppps");
 
     println!("With text:\n{file_content}");
+}
+
+struct Config {
+    query:      String,
+    file_path:  String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path }
 }
