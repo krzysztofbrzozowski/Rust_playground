89210a65c242de48636050b00f0c1d720181d89d
Krzysztof Brzozowski <krzysztof_brzozowski@icloud.com>
2024-10-11 22:28:44 +0200

    Added some logic to minigrep

--- 14_io_project_grep/minigrep/src/main.rs
+++ 14_io_project_grep/minigrep/src/main.rs
@@ -1,18 +1,23 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     // Iterators produce a series of values, and we can call the collect method on an iterator to turn it into a collection
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        // Funny issue if we not "use std::process" ->
+        // mismatched types
+        // expected `Config`, found `()`rustcClick for full compiler diagnostic
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let file_content = fs::read_to_string(config.file_path).expect("Uppps");
-
-    println!("With text:\n{file_content}");
+    run(config);
 }
 
 struct Config {
@@ -20,9 +25,22 @@ struct Config {
     file_path:  String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
+impl Config {
+    // &'static str -> will live lifetime of program, in this case point to Error
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+    
+        Ok(Config { query, file_path })
+    }
+}
 
-    Config { query, file_path }
+fn run(config: Config) {
+    let content = fs::read_to_string(config.file_path)
+        .expect("Unable to read the file {config.file_path}");
+
+    println!("File content is {content}");
 }
